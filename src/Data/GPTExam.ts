9f50da9201c74d6bd4e5ca4783e2134801efc715
KHOITRINH03 <Johntrinhnhat@gmail.com>
2023-07-09 14:01:23 -0400

    Add: data

---
+++ src/Data/GPTExam.ts
@@ -0,0 +1,29 @@
+// import { SlEnergy } from "react-icons/sl";
+// import { BsSun } from "react-icons/Bs";
+// import { IoWarningOutline } from "react-icons/io5";
+export const gptExam = [
+  {
+    id: 0,
+    // icon: BsSun,
+    heading: "Examples",
+    content1: "Explain quantum computing in simple terms",
+    content2: "Got any creative ideas for a 10 year old's birthday?",
+    content3: "How do I make an HTTP request in Javascript?",
+  },
+  {
+    id: 1,
+    // icon: SlEnergy,
+    heading: "Capabilities",
+    content1: "Remembers what user said earlier in the conversation",
+    content2: "Allows user to provide follow-up corrections",
+    content3: "Trained to decline inappropriate requests",
+  },
+  {
+    id: 2,
+    // icon: IoWarningOutline,
+    heading: "Limitations",
+    content1: "May occasionally generate incorrect information",
+    content2: "May occasionally produce harmful instructions or biased content",
+    content3: "Limited knowledge of world and events after 2021",
+  },
+];
---
+++ src/Data/Portfolio.ts
@@ -0,0 +1,10 @@
+import test1 from "../assets/test8.webp";
+
+export const portfolio = [
+  { id: 1, heading: "tile", content: "content", image: test1 },
+  { id: 2, heading: "tile", content: "content", image: test1 },
+  { id: 3, heading: "tile", content: "content", image: test1 },
+  { id: 4, heading: "tile", content: "content", image: test1 },
+  { id: 5, heading: "tile", content: "content", image: test1 },
+  { id: 6, heading: "tile", content: "content", image: test1 },
+];
