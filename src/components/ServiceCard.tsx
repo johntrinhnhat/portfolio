4aa479ce9f606dad943ccd78ebf6d3912917c81a
KHOITRINH03 <Johntrinhnhat@gmail.com>
2023-07-09 19:30:32 -0400

    create: ServiceCard

---
+++ src/components/ServiceCard.tsx
@@ -0,0 +1,25 @@
+import { Card, CardBody, VStack, Heading, Text, Icon } from "@chakra-ui/react";
+
+interface Props {
+  icon: any;
+  heading: string;
+  content: string;
+}
+
+const ServiceCard = ({ icon, heading, content }: Props) => {
+  return (
+    <Card width="300px" height="250px" boxShadow="md">
+      <CardBody>
+        <VStack>
+          <Icon boxSize="50px" as={icon} />
+          <Heading fontSize="28px" fontFamily={"Righteous"}>
+            {heading}
+          </Heading>
+          <Text textAlign={"justify"}>{content}</Text>
+        </VStack>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default ServiceCard;
