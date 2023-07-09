031fa758c955ee724edd52a7de15efcb2ab3eae6
KHOITRINH03 <Johntrinhnhat@gmail.com>
2023-07-09 19:56:25 -0400

    install: Bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 
 import App from "./App.tsx";
 import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
