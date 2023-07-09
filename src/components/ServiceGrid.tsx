c28fe63071935debd259b5038fd7ef177bcc0e76
KHOITRINH03 <Johntrinhnhat@gmail.com>
2023-07-09 19:42:02 -0400

    update: DiamondDevider

--- src/components/ServiceGrid.tsx
+++ src/components/ServiceGrid.tsx
@@ -4,13 +4,13 @@ import { MdOutlineGpsFixed } from "react-icons/md";
 import { RiCodeView } from "react-icons/ri";
 import { SiAntdesign } from "react-icons/si";
 import ServiceCard from "./ServiceCard";
-// import DeviderCustom from "./DeviderCustom";
+import DiaDivider from "./DiaDevider";
 
 const Services = () => {
   return (
     <>
       <div id="services" className="service_grid">
-        {/* <DeviderCustom Diamond1="#2B6CB0" /> */}
+        <DiaDivider Diamond1="#2B6CB0" />
         <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} gap={20}>
           <ServiceCard
             icon={RiCodeView}
